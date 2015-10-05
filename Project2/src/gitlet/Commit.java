eb2f950afb246bc03835bc411323bb4eb7440781
troy <sabotuer99@gmail.com>
2015-10-04 21:42:29 -0600

    Added "Commit" class

--- Project2/src/gitlet/Commit.java
+++ Project2/src/gitlet/Commit.java
@@ -1,5 +1,42 @@
 package gitlet;
 
-public class Commit {
+import java.io.Serializable;
+import java.util.HashMap;
 
+@SuppressWarnings("serial")
+public class Commit implements Serializable{
+
+	private String id;
+	private String parentId;
+	private Long timeStamp;
+	private String message;
+	
+	private HashMap<String, String> filePointers;
+	
+	public Commit(){};
+	
+	public Commit(String id, String parentId, Long timeStamp, String message, HashMap<String, String> filePointers){
+		this.id = id;
+		this.parentId = parentId;
+		this.timeStamp = timeStamp;
+		this.message = message;
+		this.filePointers = filePointers;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public String getParentId() {
+		return parentId;
+	}
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public HashMap<String, String> getFilePointers() {
+		return filePointers;
+	}
+	
 }
