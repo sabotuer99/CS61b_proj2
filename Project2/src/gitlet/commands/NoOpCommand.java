afe22c933bddef7098a8a09a9f0522f458366e4f
troy <sabotuer99@gmail.com>
2015-10-11 14:23:10 -0600

    Added NoOpCommand class

---
+++ Project2/src/gitlet/commands/NoOpCommand.java
@@ -0,0 +1,17 @@
+package gitlet.commands;
+
+public class NoOpCommand implements ICommand {
+
+	@Override
+	public boolean isDangerous() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean execute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
